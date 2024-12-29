e40c573725fc3c638f46a3e030cdb2a1e7aad96c
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-12-29 18:23:40 +0300

    added ErrorMessages for easy management of error messages.

---
+++ src/main/java/com/ilyasbugra/excusegenerator/util/ErrorMessages.java
@@ -0,0 +1,7 @@
+package com.ilyasbugra.excusegenerator.util;
+
+public class ErrorMessages {
+    public static final String EMPTY_CATEGORY = "My soul may be empty but Excuse Category cannot be blank.";
+    public static final String LARGE_CATEGORY = "You cannot force more than 50 characters to a space that only can have 50 chars, please don't push your luck.";
+    public static final String CATEGORY_NOT_FOUND = "Category '%s' not found.";
+}
