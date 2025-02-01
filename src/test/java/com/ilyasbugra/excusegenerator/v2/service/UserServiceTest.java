eacc5f9c3395d3e8522a49de25ae780f9f97fed9
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2025-02-01 20:27:32 +0300

    fix(test)(commit-of-shame): No one told me that the @ExtendWith and @BeforeEach part was doing the same think twice!
    - That two combined making the test -will commit after this- fail.
    - Fixed it but learned it the hard way!

--- src/test/java/com/ilyasbugra/excusegenerator/v2/service/UserServiceTest.java
+++ src/test/java/com/ilyasbugra/excusegenerator/v2/service/UserServiceTest.java
@@ -2,12 +2,9 @@ package com.ilyasbugra.excusegenerator.v2.service;
 
 import com.ilyasbugra.excusegenerator.v2.mapper.UserMapper;
 import com.ilyasbugra.excusegenerator.v2.repository.UserRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -25,16 +22,4 @@ public class UserServiceTest {
 
     @InjectMocks
     private UserService userService;
-
-    private AutoCloseable closeable;
-
-    @BeforeEach
-    public void setUp() {
-        closeable = MockitoAnnotations.openMocks(this);
-    }
-
-    @AfterEach
-    public void tearDown() throws Exception {
-        closeable.close();
-    }
 }
