9d5a148aa3826fec14b18b21e58d9d6bd467e19d
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2025-02-01 14:27:46 +0300

    test(user): set up UserServiceTest with proper resource management 🛠️
    - Mocked `UserRepository` and `PasswordEncoder` for isolated unit testing.
    - Fixed Mockito warning by properly closing `openMocks()`.
    - For anyone wondering why I add the @AfterEach and closing the closeable, there were yellow squiggles under the MokitoAnnotations.openMocks(this) line... I do believe that what I'm doing is right (no more yellow squiggles) buuuut I have no idea how much benefit there is... your lovely "I have no idea about what I'm doing" level developer xoxo
    - Next step: Testing user registration. 🔥

--- src/test/java/com/ilyasbugra/excusegenerator/v2/service/UserServiceTest.java
+++ src/test/java/com/ilyasbugra/excusegenerator/v2/service/UserServiceTest.java
@@ -1,4 +1,36 @@
 package com.ilyasbugra.excusegenerator.v2.service;
 
+import com.ilyasbugra.excusegenerator.v2.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    private AutoCloseable closeable;
+
+    @BeforeEach
+    public void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        closeable.close();
+    }
 }
