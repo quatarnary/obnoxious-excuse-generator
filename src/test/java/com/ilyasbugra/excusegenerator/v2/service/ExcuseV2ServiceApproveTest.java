988208c62d61eadddc29d6ab806628ac29ec8834
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2025-02-08 22:43:07 +0300

    test(service): verify admin approval power in testApprove_Admin_Not_CreatedBySelf ✅

--- src/test/java/com/ilyasbugra/excusegenerator/v2/service/ExcuseV2ServiceApproveTest.java
+++ src/test/java/com/ilyasbugra/excusegenerator/v2/service/ExcuseV2ServiceApproveTest.java
@@ -2,27 +2,25 @@ package com.ilyasbugra.excusegenerator.v2.service;
 
 import com.ilyasbugra.excusegenerator.model.Excuse;
 import com.ilyasbugra.excusegenerator.repository.ExcuseRepository;
+import com.ilyasbugra.excusegenerator.v2.actions.admin.AdminUser;
 import com.ilyasbugra.excusegenerator.v2.dto.ExcuseV2DTO;
 import com.ilyasbugra.excusegenerator.v2.mapper.ExcuseV2Mapper;
 import com.ilyasbugra.excusegenerator.v2.model.User;
 import com.ilyasbugra.excusegenerator.v2.model.UserRole;
-import com.ilyasbugra.excusegenerator.v2.repository.UserRepository;
+import com.ilyasbugra.excusegenerator.v2.util.ExcuseHelper;
+import com.ilyasbugra.excusegenerator.v2.util.UserHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Date;
-import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,15 +30,15 @@ public class ExcuseV2ServiceApproveTest {
     public static final String MESSAGE = "excuse-message";
     public static final String CATEGORY = "category";
     @Mock
-    Authentication authentication;
-    @Mock
-    SecurityContext securityContext;
-    @Mock
-    UserRepository userRepository;
-    @Mock
     ExcuseRepository excuseRepository;
     @Mock
     ExcuseV2Mapper excuseV2Mapper;
+    @Mock
+    UserHelper userHelper;
+    @Mock
+    ExcuseHelper excuseHelper;
+    @Mock
+    AdminUser adminUser;
     @InjectMocks
     ExcuseV2Service excuseV2Service;
     private User ADMIN_USER;
@@ -84,40 +82,31 @@ public class ExcuseV2ServiceApproveTest {
     @Test
     public void testApprove_Admin_Not_CreatedBySelf() {
         /// Arrange
-        // the ugly not unit testable part... sad developer 😔
-        SecurityContextHolder.setContext(securityContext);
-
-        when(SecurityContextHolder.getContext().getAuthentication())
-                .thenReturn(authentication);
-        when(authentication.getName())
-                .thenReturn(ADMIN_USER.getUsername());
-        when(userRepository.findByUsername(ADMIN_USER.getUsername()))
-                .thenReturn(Optional.of(ADMIN_USER));
-        when(excuseRepository.findById(EXCUSE_ID))
-                .thenReturn(Optional.of(EXCUSE));
-
-        ArgumentCaptor<Excuse> excuseCaptor = ArgumentCaptor.forClass(Excuse.class);
-
-        when(excuseRepository.save(any(Excuse.class)))
-                .thenAnswer(invocation -> invocation.getArgument(0));
-        when(excuseV2Mapper.toExcuseV2DTO(any(Excuse.class)))
+        when(userHelper.getAuthenticatedUser())
+                .thenReturn(ADMIN_USER);
+        when(excuseHelper.getExcuseById(EXCUSE_ID))
+                .thenReturn(EXCUSE);
+        doNothing()
+                .when(adminUser).approveExcuse(EXCUSE, ADMIN_USER);
+        when(excuseRepository.save(EXCUSE))
+                .thenReturn(EXCUSE);
+        when(excuseV2Mapper.toExcuseV2DTO(EXCUSE))
                 .thenReturn(EXCUSE_V2_DTO);
 
         /// Act
         ExcuseV2DTO result = excuseV2Service.approveExcuse(EXCUSE_ID);
 
-        verify(SecurityContextHolder.getContext()).getAuthentication();
-        verify(authentication, times(2)).getName();
-        verify(userRepository).findByUsername(ADMIN_USER.getUsername());
-        verify(excuseRepository).findById(EXCUSE_ID);
-        verify(excuseRepository).save(excuseCaptor.capture());
-        verify(excuseV2Mapper).toExcuseV2DTO(any(Excuse.class));
-
-        Excuse approvedExcuse = excuseCaptor.getValue();
+        verify(userHelper).getAuthenticatedUser();
+        verify(excuseHelper).getExcuseById(EXCUSE_ID);
+        verify(adminUser).approveExcuse(EXCUSE, ADMIN_USER);
+        verify(excuseRepository).save(EXCUSE);
+        verify(excuseV2Mapper).toExcuseV2DTO(EXCUSE);
 
         /// Assert
         assertNotNull(result);
-        assertEquals(ADMIN_USER.getId(), approvedExcuse.getApprovedBy().getId());
-        assertNotEquals(ADMIN_USER.getId(), approvedExcuse.getCreatedBy().getId());
+        assertNotNull(result.getId());
+        assertEquals(EXCUSE_ID, result.getId());
+        assertEquals(MESSAGE, result.getExcuseMessage());
+        assertEquals(CATEGORY, result.getCategory());
     }
 }
